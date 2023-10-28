ff8719c22b970b4e2a00e6b8d3de015ce809c6ec
Kelvin <qelvynn@gmail.com>
2023-10-27 21:04:21 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - header file for main project
+ *
+ * This file contains the prototypes of the functions
+ * used in the main project, such as _putchar and _atoi.
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
