510e6f4acb0bea8d89107b78df64b47e1134103f
Kelvin <qelvynn@gmail.com>
2023-11-22 16:39:34 -0800

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,6 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
 
 /**
@@ -11,8 +13,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -32,3 +34,4 @@ size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
+
