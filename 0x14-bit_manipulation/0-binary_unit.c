e0c2a7e0193ae32febf80dcf3622c82ffa7b161e
Kelvin <qelvynn@gmail.com>
2023-11-24 09:57:06 -0800

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_unit.c
+++ 0x14-bit_manipulation/0-binary_unit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * binary_to_uint - function converts a binary number to an unsigned int
- * @b: pointer to a string containing a binary number
+ * binary_to_uint - function converts binary number to an unsigned int
+ * @b: pointer to string containing a binary number
  *
  * Return: unsigned int with decimal value of binary number, or 0 if error
  */
