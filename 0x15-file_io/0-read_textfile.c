d4994eab9a10d6d461bd986832efabf8a90e69a0
Kelvin <qelvynn@gmail.com>
2023-11-28 07:50:24 -0800

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile- Display text file content to standard output.
+ * @filename: text file to be displayed
+ * @letters: number of characters to be displayed
+ * Return: w- actual number of bytes displayed
+ *        0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
