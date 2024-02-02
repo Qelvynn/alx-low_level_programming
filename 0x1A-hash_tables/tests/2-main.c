63112a5c7658d445271a5e7c9fb4807155cabe4c
Kelvin <qelvynn@gmail.com>
2024-02-02 15:50:18 -0800

    0x1A-hash_tables

--- 0x1A-hash_tables/tests/2-main.c
+++ 0x1A-hash_tables/tests/2-main.c
@@ -17,11 +17,14 @@ int main(void)
 	s = "cisfun";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+
 	s = "Don't forget to tweet today";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+
 	s = "98";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
 	printf("%lu\n", key_index((unsigned char *)s, hash_table_array_size));
+
 	return (EXIT_SUCCESS);
 }
